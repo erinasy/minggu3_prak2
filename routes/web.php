89b732f8d63f618230c08cfb8158b022975d02a3
erinasy <erinaseviyanti02@gmail.com>
2021-09-17 00:16:11 +0700

    modif1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',[HomeController::class,'home']);
+Route::get('/profile',[ProfileController::class,'profile']);
